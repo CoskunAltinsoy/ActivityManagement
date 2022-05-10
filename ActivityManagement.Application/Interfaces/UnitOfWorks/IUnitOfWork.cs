8c6f69808790dfaafbb2012d15e093270725ed4c
Coşkun Altınsoy <coskun.altinsoyy@gmail.com>
2022-05-10 14:05:33 +0300

    UOW Added

---
+++ ActivityManagement.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using ActivityManagement.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityManagement.Application.Interfaces.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IActivityRepository Activities { get; }
+        ICategoryRepository Categories { get; }
+        ICityRepository Cities { get; }
+        ICompanyRepository Companies { get; }
+        IUserRepository Users { get; }
+        bool SaveChanges();
+    }
+}
---
+++ ActivityManagement.Infrastructure/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using ActivityManagement.Application.Interfaces.Repositories;
+using ActivityManagement.Application.Interfaces.UnitOfWorks;
+using ActivityManagement.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityManagement.Infrastructure.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ActivityManagementDbContext _context;
+        public IActivityRepository Activities { get; }
+
+        public ICategoryRepository Categories { get; }
+
+        public ICityRepository Cities { get; }
+
+        public ICompanyRepository Companies { get; }
+
+        public IUserRepository Users { get; }
+        public UnitOfWork(IActivityRepository activities, ICategoryRepository categories, ICityRepository cities,
+                          ICompanyRepository companies, IUserRepository users, ActivityManagementDbContext context)
+        {
+            Activities = activities;
+            Categories = categories;
+            Cities = cities;
+            Companies = companies;
+            Users = users;
+            _context = context;
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
